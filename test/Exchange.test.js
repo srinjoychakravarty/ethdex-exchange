2734f42061bc781009cc987bb4c9602c1b17a2eb
subzero <chaxz93@gmail.com>
2019-07-21 23:39:52 +0300

    :tada: release(Exchange.sol): deposit token works successfully with chai test passing

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,17 +1,23 @@
 import {tokens, EVM_REVERT, INVALID_ADDRESS, INVALID_EXCHANGE} from './helpers.js'
 
 const Exchange = artifacts.require('./Exchange') 
+const Token = artifacts.require('./Token')
 
 require('chai').use(require('chai-as-promised')).should()
 
-contract('Exchange', ([deployer, feeReceiver]) => {
+contract('Exchange', ([deployer, feeReceiver, kinKendall]) => {
 
 	let exchange
+	let token
 	const exchangeCut = 4
 
 	beforeEach(async() => {
 		//Sets up exchange for all tests
 		exchange = await Exchange.new(feeReceiver, exchangeCut)
+		//Sets up dexcoin as the sample erc20 token for all tests
+		token = await Token.new()
+		// Gives kin the user some tokens to trade with on the exchange
+		token.transfer(kinKendall, tokens(25), {from: deployer})
 	})
 
 	describe('deployment', () => {
@@ -24,6 +30,35 @@ contract('Exchange', ([deployer, feeReceiver]) => {
 		it('tracks the fee percentage', async() => {
 			const feePercent = await exchange.feePercent()
 			feePercent.toString().should.equal(exchangeCut.toString())
+		})
+	})
+
+	describe('depositing tokens', () => {
+
+		let exchangeDeposit
+		let testAmount
+
+		beforeEach(async() => {
+			testAmount = tokens(7)
+			await token.approve(exchange.address, testAmount, {from: kinKendall})
+			const exchangeDeposit = await exchange.depositToken(token.address, testAmount, {from: kinKendall})
+		})
+
+		describe('successful deposit', () => {
+
+			it('tracks the token deposit', async() => {
+				// Checks token balance on exchange
+				let tokenBalance
+				tokenBalance = await token.balanceOf(exchange.address)
+				tokenBalance.toString().should.equal(testAmount.toString())
+			})
+		})
+
+		describe('failed deposit', () => {
+
+			it('xxxx', async() => {
+
+			})
 		})
 	})
 })
